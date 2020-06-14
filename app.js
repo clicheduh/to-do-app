30adf0327f8467c18fe7e6573f4b314098d793d9
Aditya Shrivastav <shriadishri@gmail.com>
2020-06-14 14:16:16 +0530

    animation changed

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var inputTodo = $("#todo-input input");
 var addButton = $("#todo-input button");
-var newTodo,
-  newHtml,
-  counter = 0;
+var newTodo, newHtml;
 
 // Add new To-do
 addButton.on("click", () => {
@@ -18,7 +16,7 @@ addButton.on("click", () => {
     $(".wrapper").append(newHtml);
     $(".wrapper div:last p").prepend(newTodo).addClass("trash");
   } else {
-    $("input")
+    $("input") // rapid blinking of input field
       .delay(500)
       .fadeTo(200, 0.2)
       .fadeTo(200, 0.8)
@@ -31,10 +29,11 @@ addButton.on("click", () => {
       .fadeTo(200, 0.2)
       .fadeTo(200, 1);
 
-    var success1 = $("#myGif").slideToggle(500).delay(2000).slideToggle(500);
+    $("#myGif").show(500).delay(2000).hide(500); // animating the gif
   }
-  inputTodo[0].value = "";
+  inputTodo[0].value = ""; // clearing input field after clicking "add to-do" button
 
+  // Delete To-do
   $(".trash").on("click", (event) => {
     event.target.parentElement.parentElement.remove();
   });
